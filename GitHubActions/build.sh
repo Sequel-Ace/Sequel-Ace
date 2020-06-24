1eb9477e097a0a196093e9a70dfee2883b662921
Jakub Kaspar <kaspikk@gmail.com>
2020-06-24 23:18:27 +0200

    Update header

--- GitHubActions/build.sh
+++ GitHubActions/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-#
-# MIT License
-#
-# Copyright (c) 2017-2020 MessageKit
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
