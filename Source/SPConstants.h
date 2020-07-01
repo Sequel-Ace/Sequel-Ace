937a205605775c3801fea1c7c55b7fdb96519dd2
James Stout <stoutyhk@gmail.com>
2020-07-01 16:59:02 +0800

    Wrapped SPLog() in DEBUG macro

--- Source/SPConstants.h
+++ Source/SPConstants.h
@@ -676,7 +676,11 @@ void _SPClear(id *addr);
 #define SPClear(x) _SPClear(&x)
 
 // Stolen from Stack Overflow: http://stackoverflow.com/questions/969130
-#define SPLog(fmt, ...) NSLog((@"%s:%d: " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#ifdef DEBUG
+#   define SPLog(fmt, ...) NSLog((@"%s:%d: " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#   define SPLog(...)
+#endif
 
 // See http://stackoverflow.com/questions/4415524
 #define COUNT_OF(x) (NSInteger)((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
