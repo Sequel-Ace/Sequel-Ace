7686700ba12445c577db64f890ce4ef4bc52ff6d
James Stout <stoutyhk@gmail.com>
2020-10-21 16:56:31 +0800

    switched highlight color to systemTealColor

--- Source/Controllers/MainViewControllers/SPBracketHighlighter.m
+++ Source/Controllers/MainViewControllers/SPBracketHighlighter.m
@@ -49,7 +49,7 @@
 	self.textView = textView;
 	self.pos1 = NSNotFound;
 	self.pos2 = NSNotFound;
-	self.highlightColor = [NSColor colorWithRed: 0xe6/255.0 green: 0xc9 /255.0 blue:0x09/255.0 alpha:1.0];
+	self.highlightColor = [NSColor systemTealColor];
 	self.enabled = YES;
 	return self;
 }
