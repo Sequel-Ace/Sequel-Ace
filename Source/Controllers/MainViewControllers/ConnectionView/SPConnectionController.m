33c9948afe0dd899886fd18a44805f0bd3c61b8c
Jakub Kaspar <kaspikk@gmail.com>
2020-11-07 21:21:17 +0100

    Revert "Merge branch 'fonts-fix3' into fonts"
    This reverts commit b6ee86de64f50119d004082371c93b293da20169, reversing
    changes made to 874db40fb20ab2403873cea242d555699eee2dc2.

--- Source/Controllers/MainViewControllers/ConnectionView/SPConnectionController.m
+++ Source/Controllers/MainViewControllers/ConnectionView/SPConnectionController.m
@@ -792,33 +792,26 @@ static NSComparisonResult _compareFavoritesUsingKey(id favorite1, id favorite2, 
 
 	// Use a magic number which needs to be added to the form when calculating resizes -
 	// including the height of the button areas below.
-	NSInteger additionalFormHeight = 90;
-	NSInteger additionalFormHeightSSL = 40;
+	NSInteger additionalFormHeight = 92;
 
 	frameRect = [connectionResizeContainer frame];
 
 	switch (theType) {
 		case SPTCPIPConnection:
 			targetResizeRect = [standardConnectionFormContainer frame];
-			if ([self useSSL]) additionalFormHeight += [standardConnectionSSLDetailsContainer frame].size.height + additionalFormHeightSSL;
+			if ([self useSSL]) additionalFormHeight += [standardConnectionSSLDetailsContainer frame].size.height;
 			break;
 		case SPSocketConnection:
 			targetResizeRect = [socketConnectionFormContainer frame];
-			if ([self useSSL]) additionalFormHeight += [socketConnectionSSLDetailsContainer frame].size.height + additionalFormHeightSSL;
+			if ([self useSSL]) additionalFormHeight += [socketConnectionSSLDetailsContainer frame].size.height;
 			break;
 		case SPSSHTunnelConnection:
 			targetResizeRect = [sshConnectionFormContainer frame];
-			if ([self useSSL]) additionalFormHeight += [sshConnectionSSLDetailsContainer frame].size.height + additionalFormHeightSSL;
+			if ([self useSSL]) additionalFormHeight += [sshConnectionSSLDetailsContainer frame].size.height;
 			break;
 	}
 
 	frameRect.size.height = targetResizeRect.size.height + additionalFormHeight;
-
-	for (NSLayoutConstraint *constraint in connectionResizeContainer.constraints) {
-		if ([constraint.identifier isEqualToString: @"connectionViewHeight"]) {
-			constraint.constant = frameRect.size.height;
-		}
-	}
 
 	if (animate && initComplete) {
 		[[connectionResizeContainer animator] setFrame:frameRect];
