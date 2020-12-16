4c0776a25b185ca1d72eef87ebe2747a8d0915b5
James Stout <stoutyhk@gmail.com>
2020-12-16 19:30:02 +0800

    WIP - doesn’t work

--- Source/Controllers/Other/SecureBookmark.swift
+++ Source/Controllers/Other/SecureBookmark.swift
@@ -16,27 +16,47 @@ struct SecureBookmark: Codable {
 }
 
 
-//extension SecureBookmark {
-//    func encode() -> Data {
-//        let data = NSMutableData()
-//        let archiver = NSKeyedArchiver(forWritingWith: data)
-//        archiver.encode(bookmarkData, forKey: "bookmarkData")
-//        archiver.encode(options, forKey: "options")
-//        archiver.encode(theUrl, forKey: "theUrl")
-//        archiver.finishEncoding()
-//        return data as Data
-//    }
-//
-//    init?(data: Data) {
-//        let unarchiver = NSKeyedUnarchiver(forReadingWith: data)
-//        defer {
-//            unarchiver.finishDecoding()
-//        }
-//        guard let bookmarkData = unarchiver.decodeObject(forKey: "bookmarkData") as? Data else { return nil }
-//        guard let theUrl = unarchiver.decodeObject(forKey: "theUrl") as? URL else { return nil }
-//        options = unarchiver.decodeDouble(forKey: "options")
-//        self.bookmarkData = bookmarkData
-//        self.theUrl = theUrl
-//    }
-//}
+extension SecureBookmark {
+    func encode() -> Data {
+        let data = NSMutableData()
+        let archiver = NSKeyedArchiver(forWritingWith: data)
+        archiver.encode(NSKeyedArchiver.archivedData(withRootObject: bookmarkData), forKey: "bookmarkData")
+        archiver.encode(NSKeyedArchiver.archivedData(withRootObject: options), forKey: "options")
+        archiver.encode(NSKeyedArchiver.archivedData(withRootObject: theUrl), forKey: "theUrl")
+        archiver.finishEncoding()
+        return data as Data
+    }
+
+    init?(data: Data) {
+        let unarchiver = NSKeyedUnarchiver(forReadingWith: data)
+        defer {
+            unarchiver.finishDecoding()
+        }
+        guard
+            let bookmarkData = unarchiver.decodeObject(forKey: "bookmarkData") as? Data,
+            let unarchivedBookmarkData = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(bookmarkData) as? Data
+        
+        else {
+            return nil
+        }
+        guard
+            let theUrl = unarchiver.decodeObject(forKey: "theUrl") as? Data,
+            let unarchivedURL = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(theUrl) as? URL
+        
+        else {
+            return nil
+        }
+        guard
+            let options = unarchiver.decodeObject(forKey: "options") as? Data,
+            let unarchivedOptions = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(options) as? Double
+        
+        else {
+            return nil
+        }
+        
+        self.bookmarkData = unarchivedBookmarkData
+        self.theUrl = unarchivedURL
+        self.options = unarchivedOptions
+    }
+}
 
