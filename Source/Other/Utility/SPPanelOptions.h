ee83d4fa25b0861bed60ad9083dc4067b33d8f03
James Stout <stoutyhk@gmail.com>
2021-01-05 22:55:03 +0800

    added index

--- Source/Other/Utility/SPPanelOptions.h
+++ Source/Other/Utility/SPPanelOptions.h
@@ -19,6 +19,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (readwrite, assign) BOOL canChooseDirectories;
 @property (readwrite, assign) BOOL allowsMultipleSelection;
 @property (readwrite, assign) BOOL isForStaleBookmark;
+@property (readwrite, assign) NSUInteger index;
 @property (readwrite, copy, nullable) NSMutableArray<NSString *> *fileNames;
 
 - (NSString *)jsonStringWithPrettyPrint:(BOOL)prettyPrint;
