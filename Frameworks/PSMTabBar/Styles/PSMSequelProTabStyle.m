545c59d94bc34069d6755ddc0d602b188d2913ae
James Stout <stoutyhk@gmail.com>
2021-01-17 22:36:05 +0800

    rm logging

--- Frameworks/PSMTabBar/Styles/PSMSequelProTabStyle.m
+++ Frameworks/PSMTabBar/Styles/PSMSequelProTabStyle.m
@@ -571,10 +571,6 @@
 	NSColor *fillColor = nil;
     NSColor *cellBackgroundColor = [[cell backgroundColor] copy]; // seems to be almost always nil, but take a copy as we've seen memory issues
 
-    if(cellBackgroundColor){
-        SPLog(@"cellBackgroundColor not nil: %@", cellBackgroundColor.description);
-        CLS_LOG(@"cellBackgroundColor not nil: %@", cellBackgroundColor.description);
-    }
 	// Set up colours
 	if (([[tabBar window] isMainWindow] || [[[tabBar window] attachedSheet] isMainWindow]) && [NSApp isActive]) {
 		if ([cell state] == NSOnState) { //active window, active cell
