6535bf4f3ae9f64a8eacd28d174fcbc475a0b58c
James Stout <stoutyhk@gmail.com>
2021-02-11 05:05:22 +0800

    added version compare funcs

--- Source/Other/Extensions/StringExtension.swift
+++ Source/Other/Extensions/StringExtension.swift
@@ -76,6 +76,43 @@ extension String {
 	var trimmedString: String {
         return self.trimmingCharacters(in: .whitespacesAndNewlines)
     }
+
+    func getRawVersionString() -> String? {
+        return self.dropPrefix("v")
+            .split(separator: "-").map { String($0) }[0]
+
+    }
+
+    // Modified from the DragonCherry extension - https://github.com/DragonCherry/VersionCompare
+    private func compare(toVersion targetVersion: String) -> ComparisonResult {
+        let versionDelimiter = "."
+        var result: ComparisonResult = .orderedSame
+        var versionComponents = components(separatedBy: versionDelimiter)
+        var targetComponents = targetVersion.components(separatedBy: versionDelimiter)
+
+        while versionComponents.count < targetComponents.count {
+            versionComponents.append("0")
+        }
+        while targetComponents.count < versionComponents.count {
+            targetComponents.append("0")
+        }
+
+        for (version, target) in zip(versionComponents, targetComponents) {
+            result = version.compare(target, options: .numeric)
+            if result != .orderedSame {
+                break
+            }
+        }
+
+        return result
+    }
+
+    func isVersion(equalTo targetVersion: String) -> Bool { return compare(toVersion: targetVersion) == .orderedSame }
+    func isVersion(greaterThan targetVersion: String) -> Bool { return compare(toVersion: targetVersion) == .orderedDescending }
+    func isVersion(greaterThanOrEqualTo targetVersion: String) -> Bool { return compare(toVersion: targetVersion) != .orderedAscending }
+    func isVersion(lessThan targetVersion: String) -> Bool { return compare(toVersion: targetVersion) == .orderedAscending }
+    func isVersion(lessThanOrEqualTo targetVersion: String) -> Bool { return compare(toVersion: targetVersion) != .orderedDescending }
+
 }
 
 @objc extension NSString {
