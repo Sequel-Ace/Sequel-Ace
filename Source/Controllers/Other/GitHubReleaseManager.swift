a05c49a415492d0e05b75feac2cc61ae2d6c68d0
James Stout <stoutyhk@gmail.com>
2021-02-13 21:34:08 +0800

    various updates
    rm Ink
    localized strings
    rm generate release notes
    rm NSProgressIndicator

--- Source/Controllers/Other/GitHubReleaseManager.swift
+++ Source/Controllers/Other/GitHubReleaseManager.swift
@@ -9,7 +9,6 @@
 import Foundation
 import OSLog
 import Alamofire
-import Ink
 
 @objc final class GitHubReleaseManager: NSObject {
     static let NSModalResponseView    : NSApplication.ModalResponse = NSApplication.ModalResponse(rawValue: 1001);
@@ -62,7 +61,6 @@ import Ink
 
         Log.debug("checkReleaseWithName: \(name)")
 
-
         let urlStr = GitHubReleaseManager.githubURLStr.format(user, project)
 
         Log.debug("GitHubReleaseManager.config = \(String(describing: GitHubReleaseManager.config))")
@@ -79,9 +77,14 @@ import Ink
                     Log.info("Validation Successful")
 
                     do{
-                        let json = try JSONSerialization.jsonObject(with: response.data!, options: JSONSerialization.ReadingOptions()) // FIXME: data!
-                        let prettyData = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
-                        let gitHub = try GitHub(data: prettyData)
+                        guard let responseData = response.data else {
+                            Log.error("response.data not valid")
+                            return
+                        }
+                        
+                        let json = try JSONSerialization.jsonObject(with: responseData, options: JSONSerialization.ReadingOptions())
+                        let jsonData = try JSONSerialization.data(withJSONObject: json, options: .fragmentsAllowed)
+                        let gitHub = try GitHub(data: jsonData)
 
                         var releasesArray = gitHub.sorted(by: { (element0: GitHubElement, element1: GitHubElement) -> Bool in
                             return element0 > element1
@@ -95,7 +98,7 @@ import Ink
                                  return
                             }
                             self.currentReleaseName = currentReleaseName
-                            Log.debug("Found release at index:[\(i)] name: \(currentReleaseName))")
+                            Log.debug("Found this release at index:[\(i)] name: \(currentReleaseName))")
                         }
 
                         if includeDraft == false {
@@ -120,6 +123,7 @@ import Ink
                             }
                             self.availableReleaseName = availableReleaseName
                             Log.info("Found availableRelease: \(availableReleaseName)")
+                            // ??? do we need this?
                             NotificationCenter.default.post(name: Notification.Name(NSNotification.Name.SPNewReleaseAvailable.rawValue), object: availableRelease)
                             _ = self.displayNewReleaseAvailableAlert()
                         }
@@ -166,17 +170,18 @@ import Ink
             asset = availableRelease?.assets[i] ?? nil
         }
 
-        message = "Version %@ is available. You are currently running %@" .format(availableReleaseName, currentReleaseName)
+        message = NSLocalizedString("Version %@ is available. You are currently running %@",
+                                    comment: "Version %@ is available. You are currently running %@") .format(availableReleaseName, currentReleaseName)
 
         let alert = NSAlert()
-        alert.messageText = "A new version is available"
+        alert.messageText = NSLocalizedString("A new version is available", comment: "A new version is available")
         alert.informativeText = message
         alert.showsSuppressionButton = true
         alert.alertStyle = .informational
         alert.addButton(withTitle: "View").tag = GitHubReleaseManager.NSModalResponseView.rawValue
         
         if asset != nil {
-            alert.addButton(withTitle: "Download").tag = GitHubReleaseManager.NSModalResponseDownload.rawValue
+            alert.addButton(withTitle: NSLocalizedString("Download", comment: "Download new version")).tag = GitHubReleaseManager.NSModalResponseDownload.rawValue
         }
         alert.addButton(withTitle: NSLocalizedString("Cancel", comment: "cancel button")).tag = NSApplication.ModalResponse.cancel.rawValue
 
@@ -196,10 +201,9 @@ import Ink
                     NSWorkspace.shared .open(localURL)
                 case GitHubReleaseManager.NSModalResponseDownload:
                     self.Log.debug("user clicked download")
-                    self.downloadNewRelease(asset: asset!)
+                    self.downloadNewRelease(asset: asset!) // already checked that this is not nil
                 case NSApplication.ModalResponse.cancel:
                     self.Log.debug("user clicked cancel")
-                    self.generateReleaseNoteFromRelease(fromRelease: currentRelease!, toRelease: availableRelease!)
                 default:
                     return
             }
@@ -224,18 +228,6 @@ import Ink
             return (fileURL, [.removePreviousFile, .createIntermediateDirectories])
         }
 
-        guard let mainWindow = NSApp.mainWindow else { return }
-
-        let progressbar = NSProgressIndicator()
-        progressbar.frame = NSRect(x: 100, y: 200, width: 150, height: 100)
-        progressbar.minValue = 0.0
-        progressbar.maxValue = 1.0
-        progressbar.isIndeterminate = false
-        progressbar.isDisplayedWhenStopped = false
-
-        mainWindow.contentView?.addSubview(progressbar)
-        progressbar.startAnimation(self)
-                    
         var previousFractionCompleted : Double = 0.0
 
         AF.download(asset.browserDownloadURL, to: destination)
@@ -243,15 +235,13 @@ import Ink
                 self.Log.debug("Download Progress: \(progress.fractionCompleted)")
                 previousFractionCompleted = progress.fractionCompleted - previousFractionCompleted
                 self.Log.debug("previousFractionCompleted: \(previousFractionCompleted)")
-                progressbar.increment(by: previousFractionCompleted)
                 previousFractionCompleted = progress.fractionCompleted
                 if progress.fractionCompleted == 1.0 {
                     self.Log.debug("Download Complete")
-                    progressbar.stopAnimation(self)
-                    progressbar.removeFromSuperview()
                 }
-            }.response { response in
-                debugPrint(response)
+            }
+            .response { response in
+                self.Log.debug("response: \(response)")
 
                 if response.error == nil, let filePath = response.fileURL?.path {
                     self.Log.debug("downloadNewRelease: \(filePath)")
@@ -259,122 +249,4 @@ import Ink
             }
     }
 
-    private func generateReleaseNoteFromRelease(fromRelease: GitHubElement, toRelease: GitHubElement ){
-
-        guard
-            var fromIndex : Int = releases.firstIndex(of: fromRelease),
-            fromIndex != NSNotFound
-        else {
-            Log.error("Release not found: \(fromRelease)")
-            return
-        }
-
-        guard
-            var toIndex : Int = releases.firstIndex(of: toRelease),
-            toIndex != NSNotFound
-        else {
-            Log.error("Release not found: \(toRelease)")
-            return
-        }
-
-        self.Log.debug("toIndex: \(toIndex)")
-        self.Log.debug("fromIndex: \(fromIndex)")
-
-        if fromIndex < toIndex {
-            self.Log.debug("reversing release array")
-
-            let tmp = fromIndex
-            fromIndex = toIndex
-            toIndex = tmp
-            releases.reverse()
-        }
-
-
-        self.Log.debug("toIndex: \(toIndex)")
-        self.Log.debug("fromIndex: \(fromIndex)")
-
-        var matchedIndices: [Int] = []
-        for (i, _) in zip(releases.indices, releases) {
-            self.Log.debug("i: \(i)")
-
-            if i <= fromIndex && i >= toIndex{
-                matchedIndices.append(i)
-            }
-        }
-
-        self.Log.debug("matchedIndices: \(matchedIndices)")
-
-        let result = NSMutableAttributedString()
-        let newLine = NSAttributedString(string: "\n")
-
-        result.beginEditing()
-
-        for (_, value) in matchedIndices.enumerated() {
-
-            self.Log.debug("value: \(value)")
-            self.Log.debug("GitHubElement: \(releases[value].name)")
-
-            if result.length > 0 {
-                result.append(newLine)
-                result.append(newLine)
-            }
-
-            result.append(NSAttributedString(string: "## " + releases[value].name, attributes: nil))
-            result.append(newLine)
-            result.append(newLine)
-            let lines = releases[value].body.separatedIntoLines()
-
-            Log.debug("lines: [\(lines)]")
-
-            var newLinesArray : [String] = []
-            do{
-                for str in lines {
-                    var newStr = str.trimmedString
-                    if newStr.count == 0 {
-                        continue
-                    }
-                    if newStr.hasPrefix("###") {
-                        newStr.append("\n")
-                        newStr.insert("\n", at: newStr.startIndex)
-                    }
-
-                    let searchText = newStr
-                    let regex = try NSRegularExpression(pattern: "(-\\s)(\\w*)(.*)", options: [])
-                    let replacedText = regex.stringByReplacingMatches(in: searchText, options: [], range: NSRange(searchText.startIndex..<searchText.endIndex, in: searchText), withTemplate: "$1`$2`$3")
-
-                    self.Log.debug("replacedText: \(replacedText)")
-
-                    newLinesArray.append(replacedText)
-                }
-            }
-            catch{
-
-            }
-
-            self.Log.debug("newLinesArray: \(newLinesArray)")
-
-
-            let newLinesNSArray = newLinesArray as NSArray
-
-
-            let newStr = newLinesNSArray.componentsJoined(by: "\n")
-            self.Log.debug("newStr: \(newStr)")
-
-            result.append(NSAttributedString(string: newStr, attributes: nil))
-
-
-        }
-
-        result.endEditing()
-        self.Log.debug("result: \(result)")
-
-        let markdown: String = result.mutableString as String
-        let parser = MarkdownParser()
-        let html = parser.html(from: markdown)
-
-        self.Log.debug("html: \(html)")
-
-
-
-    }
 }
